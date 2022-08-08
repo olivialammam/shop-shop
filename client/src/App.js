a4651195ac7f0eb0a68228181e807f4d45021941
Olivia Lammam <olivia.l7475@gmail.com>
2022-08-08 01:45:24 -0700

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
@@ -14,7 +10,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+import { Provider } from 'react-redux';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -37,48 +33,31 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          {/*<Provider>*/}
+          <Provider store={store}>
             <Nav />
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              />
-              <Route 
-                path="/success" 
-                element={<Success />} 
-              />
-              <Route 
-                path="/orderHistory" 
-                element={<OrderHistory />} 
-              />
-              <Route 
-                path="/products/:id" 
-                element={<Detail />} 
-              />
-              <Route 
-                path="*" 
-                element={<NoMatch />} 
-              />
-            </Routes>
-          </StoreProvider>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/success" component={Success} />
+              <Route component={NoMatch} />
+              
+            </Switch>
+          </Provider>
+          {/*</Provider>*/}
         </div>
       </Router>
     </ApolloProvider>
   );
 }
-
 export default App;
+
